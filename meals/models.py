604fec6d266f6cfe87611f3e7ff049ac33c3e4db
Nicole M <nicmatts@gmail.com>
2014-07-18 18:11:36 -0500

    Move category choices to meals model

--- meals/models.py
+++ meals/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 
+
 # Create your models here.
 class AllMealsManager(models.Manager):
     def get_queryset(self):
@@ -9,18 +10,22 @@ class AllIngredientsManager(models.Manager):
     def get_queryset(self):
         return super(AllIngredientsManager, self).get_queryset().all()
 
-class Ingredient(models.Model):
-	CATEGORY = (
-		('breakfast', 'breakfast'),
-		('chicken', 'chicken'),
-		('hotdish', 'hotdish'),
-		('pasta', 'pasta'),
-		('soup', 'soup'),
-		('other', 'other'),
-	)
+class AllLastMadesManager(models.Manager):
+    def get_queryset(self):
+        return super(AllLastMadesManager, self).get_queryset().all()
 
-	ingredient_name = models.CharField(max_length = 200, verbose_name = "Ingredient name")
-	ingredient_category = models.CharField(max_length=15, choices=CATEGORY)
+class LastTenMadesManager(models.Manager):
+    def get_queryset(self):
+        return super(LastTenMadesManager, self).get_queryset().all().order_by('-id')[:10]
+
+class LeastTenMadesManager(models.Manager):
+    def get_queryset(self):
+        return super(LeastTenMadesManager, self).get_queryset().all().order_by('id')[:10]
+
+
+class Ingredient(models.Model):
+	ingredient_name = models.CharField(max_length=200, verbose_name="Ingredient name")
+	ingredient_quantity = models.CharField(max_length=5, verbose_name="Ingredient quantity", blank="True")
 
 	def __unicode__(self):
 		return self.ingredient_name
@@ -29,10 +34,23 @@ class Ingredient(models.Model):
 
 
 class Meal(models.Model):
+	CATEGORY = (
+		('breakfast', 'breakfast'),
+		('cheese', 'cheese'),
+		('chicken', 'chicken'),
+		('hotdish', 'hotdish'),
+		('pasta', 'pasta'),
+		('pizza', 'pizza'),
+		('soup', 'soup'),
+		('other', 'other'),
+	)
+
 	meal_name = models.CharField(max_length = 200, verbose_name="Dish name")
 	meal_description = models.TextField(blank=True, verbose_name="Dish description")
+	meal_category = models.CharField(max_length=15, choices=CATEGORY)
 	meal_ingredient = models.ManyToManyField(Ingredient)
-	
+	meal_slug = models.SlugField(max_length=100, unique=True)
+
 	def __unicode__(self):
 		return self.meal_name
 
@@ -40,8 +58,13 @@ class Meal(models.Model):
 
 class LastMade(models.Model):
 	last_made = models.DateField(auto_now_add=False, auto_now=True, verbose_name="Last made on")
-	meal = models.ForeignKey(Meal)
+	meal = models.ForeignKey(Meal,related_name='meal')
+	last_made_meal_slug = models.SlugField(max_length=100, unique=True)
 
 	def __unicode__(self):
-		return self.last_made
+		return unicode(self.last_made)
+
+	all_last_mades = AllLastMadesManager()
+	last_ten_mades = LastTenMadesManager()
+	least_ten_mades = LeastTenMadesManager()
 
